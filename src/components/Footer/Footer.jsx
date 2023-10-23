ecf7ae44113bda338e7d3469d9623ebd59195f23
Anna Kondrateva <85047120+AnnaKondrDeveloper@users.noreply.github.com>
2023-10-23 17:05:06 -0400

    Added link on github

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import "./Footer.css";
+import { AiFillGithub } from "react-icons/ai";
 
 export default function Footer() {
   return (
-	 <footer className = "footer">Made with coding 🖤</footer>
+	 <footer className="footer">
+		<a className = "footer_button" href='https://github.com/AnnaKondrDeveloper/ShopNook'> 
+			<div className = "footer_button_text"><AiFillGithub /> View on GitHub</div>
+		</a>
+	 	<p className = "footer_text">Made with coding 🖤</p>
+	 </footer>
   )
 }
--- src/db/db.js
+++
@@ -1,6 +0,0 @@
-import Dexie from 'dexie';
-
-export const db = new Dexie('ShopNookApp');
-db.version(1).stores({
-  items: '++id, title, img_url, descr, category, price, count, totalPrice',
-});
